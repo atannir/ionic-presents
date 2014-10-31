d23893fd9fdf47c9be1ac1ca99be35ee6a65ce57
Mike Hartington <mikehartington@gmail.com>
2014-10-31 12:42:55 -0400

    fine tuned

--- demos/popover/index.js
+++ demos/popover/index.js
@@ -2,8 +2,17 @@ angular.module('ionicApp', ['ionic'])
 
 .controller('MainCtrl', function($scope, $ionicPopover) {
 
-  $ionicPopover.fromTemplateUrl('popover.html', function(popover) {
+
+  $ionicPopover.fromTemplateUrl('templates/popover.html', function(popover) {
     $scope.popover = popover;
   });
+
+  $scope.demo = 'ios';
+  $scope.setPlatform = function(p) {
+    document.body.classList.remove('platform-ios');
+    document.body.classList.remove('platform-android');
+    document.body.classList.add('platform-' + p);
+    $scope.demo = p;
+  }
 
 });
